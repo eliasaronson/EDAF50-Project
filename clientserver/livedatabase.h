6106bb52610f89511b87874b87f324a03737aae3
eliasaronson <elias.aronson@gmail.com>
2020-04-01 15:59:24 +0200

    Live database h-file

---
+++ clientserver/livedatabase.h
@@ -0,0 +1,17 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+
+class LiveDataBase {
+public:
+  LiveDataBase() = default;
+  void addNewsgroup();
+  void deleteNewsgroup();
+  void addArtikel();
+  void removeartikel();
+  void listArtikels(); 
+private:
+  
+};
+
+#endif
