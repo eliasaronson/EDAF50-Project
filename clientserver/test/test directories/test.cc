341bbaecc5919a61ca1ebcad479ba698078cf1b7
olofk <olof-karlsson@outlook.com>
2020-04-07 10:36:06 +0200

    updated test.cc, minor changes

--- clientserver/test/test directories/test.cc
+++ clientserver/test/test directories/test.cc
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include <iostream>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -7,7 +9,7 @@ using namespace std;
 
 int main() {
    int status;
-   status = system("mkdir -p /Documents/C++/EDAF50-Project/New-folder-test"); // Creating a directory
+   status = system("mkdir -p ~/EDAF50-Project/New_folder_test"); // Creating a directory
    if (status == -1)
       cerr << "Error : " << strerror(errno) << endl;
    else
